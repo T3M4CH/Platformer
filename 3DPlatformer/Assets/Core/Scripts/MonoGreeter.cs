c774030cc5e7d9d3247deed757d9b476140a5844
Artyom <picheneg.epta@gmail.com>
2023-09-23 08:05:01 -0700

    Initial Commit

---
+++ 3DPlatformer/Assets/Core/Scripts/MonoGreeter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Core.Scripts
+{
+    public class MonoGreeter : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello!");
+        }
+    }
+}
